91e5e74877539ca6abba980e8986cd311cae6889
haeeedar6-ops <haeeedar6@gmail.com>
2026-09-07 06:08:07 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 import os
+import threading
 import telebot
 from flask import Flask, request
 
 TOKEN = "8770815242:AAHZ5-06ck0ktdx_Yw-sTlda5Bz8Dz6izEM"
 bot = telebot.TeleBot(TOKEN)
-# سيرفر وهمي بسيط لتبقى خدمة Render (Web Service) سعيدة وشغالة
+
 app = Flask(__name__)
 
 
@@ -15,22 +16,16 @@ def home():
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
-  bot.reply_to(message, "أهلاً بك في بوت أرض الحرف الخاص بفري فاير! 🔥")
+  bot.reply_to(message, "أهلاً بك في بوت أرض الحرف الخاص بفري فاير! ")
 
 
-# باقي أوامر البوت الخاصة بك هنا...
+def run_bot():
+  bot.infinity_polling()
 
-# تشغيل السيرفر الوهمي مع البوت
+
 if __name__ == "__main__":
-  import threading
-
-  # تشغيل البوت بخاصية الـ Polling في الخلفية
-  def run_bot():
-    bot.infinity_polling()
-
   t = threading.Thread(target=run_bot)
   t.start()
 
-  # تشغيل سيرفر Flask على المنفذ المطلوب من Render
   port = int(os.environ.get("PORT", 5000))
   app.run(host="0.0.0.0", port=port)
